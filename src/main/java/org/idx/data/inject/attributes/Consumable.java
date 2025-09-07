90752ae044df61c4cc43d05ec98550a91c2ca77f
Millo <23leeuw@gmail.com>
2025-09-07 19:37:29 +0200

    readded old things

--- src/main/java/org/idx/data/inject/attributes/Consumable.java
+++ src/main/java/org/idx/data/inject/attributes/Consumable.java
@@ -1,38 +1,34 @@
 package org.idx.data.inject.attributes;
 
+import org.idx.Main;
+import org.idx.enums.AttributeType;
+import org.idx.enums.Trigger;
+import org.idx.items.AttributeItem;
+import org.idx.items.components.impl.TriggersComponent;
+
 public class Consumable {
 
     public static void inject() {
 
-        // Common
-        {
+        Main.dataManager.addItem(new AttributeItem("consumable", "Consumable", "cooked_beef", AttributeType.ATTRIBUTE)
+                .setDescription("Causes the item to disappear when eaten.")
+                .addComponent(new TriggersComponent(
+                        Trigger.EAT
+                ))
+        );
+        Main.dataManager.addItem(new AttributeItem("consumed", "Consumed", "structure_void", AttributeType.ATTRIBUTE)
+                .setDescription("Causes the item to disappear when used.")
+                .addComponent(new TriggersComponent(
+                        Trigger.USE
+                ))
+        );
+        Main.dataManager.addItem(new AttributeItem("heal", "Heal", "apple", AttributeType.ATTRIBUTE)
+                .setDescription("Consuming this item heals the consumer.")
+                .addComponent(new TriggersComponent(
+                        Trigger.EAT
+                ))
+        );
 
-        }
-
-        // Uncommon
-        {
-
-        }
-
-        // Rare
-        {
-
-        }
-
-        // Epic
-        {
-
-        }
-
-        // Legendary
-        {
-
-        }
-
-        // Mythic
-        {
-
-        }
     }
 
 }
